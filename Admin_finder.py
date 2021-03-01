9c345d2fe1d1b747d0f4e4d7918ec38879b86073
Alireza <54986624+Alireza-karimi-new@users.noreply.github.com>
2021-03-01 16:12:17 +0330

    Add files via upload

---
+++ Admin_finder.py
@@ -0,0 +1,68 @@
+
+import requests
+from termcolor import colored
+import time
+import http
+import urllib3
+
+
+print("\n")
+print(r"||\\		    ||     //")
+print(r"|| \\               ||    //")
+print(r"||  \\		    ||   //")
+print(r"||   \\		    ||  //")
+print(r"||____\\	    || //")
+print(r"||_____\\	    ||//")
+print(r"||	\\	    ||\\")
+print(r"||	 \\	    || \\")
+print(r"||	  \\        ||  \\")
+print(r"||	   \\	    ||   \\")
+print(r"||	    \\	    ||    \\")
+print(r"||	     \\	    ||     \\")
+print("\n")
+
+
+print(colored("██████████████████████████████","green"))
+print(colored("██████████████████████████████","green"))
+print(colored("██████████████████████████████","white"))
+print(colored("██████████████████████████████","white"))
+print(colored("██████████████████████████████","red"))
+print(colored("██████████████████████████████","red"))
+print("\n")
+
+with open('list.txt') as f:
+    flat_list=[word for line in f for word in line.split()]
+
+
+
+
+number = 0
+for num in open("list.txt"):
+    number += 1
+
+
+
+print(colored("\n  ►►►►►►►ATTENTION◄◄◄◄◄◄◄  ","red"))
+print(colored("    Do not use / at the end of the url    ","red"))
+print(colored("    for examp https://www.examp.com \n","green"))
+Target = input(colored("Enter website url :\n","blue"))
+
+
+"""  ***main***  """
+def find():
+    for i in range(1,number):
+        print(i)
+        site = requests.get("https://www.toplearn.com" +"/"+ flat_list[i])
+        # print(site.url)
+        check = site.status_code
+        print("this web is ==> ",site.url,colored(check,"yellow"))
+        if site.status_code == 200:
+            print("this web is ==> ",site.url,colored(check,"red"))
+            time.sleep(10)
+            break
+        try:
+            pass
+        except (ConnectionError, requests.exceptions.ConnectionError , ConnectionResetError , http.client.RemoteDisconnected ,urllib3.exceptions.ProtocolError):
+                print("Something else went wrong")    
+
+find()
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Admin_panel_finder
-Admin panel finder
+HELLO my name is Alireza Karimi 
+||\\		||     //
+|| \\           ||    //
+||  \\		||   //
+||   \\		||  //
+||____\\        || //
+||_____\\       ||//
+||	\\      ||\\
+||	 \\     || \\
+||	  \\    ||  \\
+||	   \\	||   \\
+||	    \\	||    \\
+
+
+this tool made with python 3
+
+pip for install {
+	pip3 install termcolor
+	pip3 install requests
+ 	pip3 install http
+	pip3 install urllib3
+}
+
+in the end 
+python3 Admin_finder.py
+
+thanks to use 
